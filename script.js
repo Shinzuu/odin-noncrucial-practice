0a12f22bd6c6972f64761f3130b0453f918b3304
Shinzuu <not.shinzuu@gmail.com>
2024-04-06 23:17:44 +0600

    complete

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 // your JavaScript file
 const container = document.querySelector("#container");
 
+
 const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
-
 container.appendChild(content);
+
+const redParagraph = document.createElement("p");
+redParagraph.textContent = "Hey I'm red!";
+redParagraph.classList.add("red-text");
+container.appendChild(redParagraph);
+
+const blueHeading = document.createElement("h3");
+blueHeading.textContent = "I'm a blue H3!";
+blueHeading.classList.add("blue-text");
+container.appendChild(blueHeading);
+
+const newDiv = document.createElement("div");
+newDiv.style.border = "2px solid black";
+newDiv.style.backgroundColor = "pink";
+
+    const divHeading = document.createElement("h1");
+    divHeading.textContent = "I'm in a div"
+    newDiv.appendChild(divHeading);
+
+    const divParagrapph = document.createElement("p");
+    divParagrapph.textContent = "ME TOO!";
+    newDiv.appendChild(divParagrapph);
+
+container.appendChild(newDiv);
